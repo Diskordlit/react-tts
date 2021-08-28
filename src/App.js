5d711721d1e67e9722962621fb7e6bb344f12a3b
kilat9 <johariidzman@gmail.com>
2021-08-28 13:38:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react"
 
 function App() {
+  const [textListItem, setTextListItem] = useState([
+    "Item 1",
+    "Item Dua",
+    "Item San",
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <>
+          <h1>React TTS</h1>
+
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "30%",
+            transform: "translate(-50%, -50%)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h2>Text List</h2>
+          <ol>
+            {textListItem.map((text, index) => {
+              return <li key={index} style={{ textAlign: 'left'}}>{text}</li>;
+            })}
+          </ol>
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <input></input>
+          <button>Send</button>
+        </div>
+      </>
     </div>
   );
 }
