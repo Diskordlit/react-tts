56e50107d48f9e5972802fe54bbaef7d7f23d3bb
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2021-08-28 20:06:36 +0800

    Refactored the code to use TypeScript

---
+++ pages/about.ts
@@ -0,0 +1,6 @@
+function About() {
+    const text:string = 'This is the about page'
+    return text
+}
+
+export default About;
---
+++ pages/index.ts
@@ -0,0 +1,6 @@
+function HomePage() {
+    const text:string = 'Welcome to Next.js!'
+    return text
+  }
+  
+  export default HomePage
